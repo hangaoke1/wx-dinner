f02c18f544b44e47204446a2a89bca28649a0c32
hangaoke <hange@guahao.com>
2019-03-15 15:12:57 +0800

    Initial Commit

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="card">
+      {{text}}
+      自定义卡片
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style>
+.card {
+  padding: 10px;
+}
+</style>
